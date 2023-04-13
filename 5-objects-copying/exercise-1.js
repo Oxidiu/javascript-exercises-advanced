29238474645e5d7f393c82a217a8396d2c89116a
OXIDE <oxidiu777@gmail.com>
2023-04-13 19:05:30 +0300

    5-objects-copying updated

--- 5-objects-copying/exercise-1.js
+++ 5-objects-copying/exercise-1.js
@@ -4,7 +4,7 @@ const person1 = {
   age: 25,
 };
 
-const person2 = person1;
+let person2 = person1;
 
 person2.firstName = "Simon"
 
@@ -14,3 +14,11 @@ console.log(person1);
 console.log(person2);
 
 //The name is modified in both person1 and person2 objects, because they're not actually two different objects, but two different adrdresses to the same object.
+
+person2 = {...person1}; // This way one can actually copy an object and have a whole new object because this way, a new object is created and all the key-values from the last obj will be copied to it.
+person2.firstName = "Oliver";
+
+console.log(person1)
+console.log(person2) // Now we see if we modify the second object, the first one will remain intact.
+
+
