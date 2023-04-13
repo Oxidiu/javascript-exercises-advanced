0bf5ccdbb9ce5b6c59ed978aa4a90a5febc1b889
OXIDE <oxidiu777@gmail.com>
2023-04-14 00:13:04 +0300

    11-Array-Methods-Advanced 12-Array-Methods-Advanced 13-array-methods-advanced 14-array-methods-advanced

---
+++ 12-Array-Methods-Advanced/exercise-2.js
@@ -0,0 +1,21 @@
+function nicknameMap(persons) {
+  let nicknames = persons.map(persons => `${persons.name}-${persons.age}`)
+  return nicknames
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
